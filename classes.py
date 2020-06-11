193a00bacc17a0808c3dc4d496fba694c4710157
gianrathgeb <gian.rathgeb@bbz-sh.ch>
2020-06-11 15:37:42 +0200

    Updated classes.py

--- classes.py
+++ classes.py
@@ -4,28 +4,34 @@ from modules import time, keyboard
 class SpeedTester:
     def __init__(self, text):
         self.text = text
-        self.input = ""
+        self.typed_text = ""
+        self.stop_waiting = False
+        self.mode = "default"
+        self.result_time = float
 
     def start_program(self):
-        if self.wait("Press the <ENTER> key to start typing:\n" + str(self.text)):
+        self.wait("Press the <ENTER> key to start typing: " + str(self.text) + "\n")
+        if self.stop_waiting:
             self.start_typing()
 
     def wait(self, text):
         wait = input(text)
-        print(str(wait))
         if wait == "":
-            return True
+            self.stop_waiting = True
+        else:
+            self.stop_waiting = False
 
     def start_typing(self):
-        self.starting_time = time.time()
-        self.input = input("\n")
-        self.time = time.time() - self.starting_time
-        print(self.input)
-        print(self.time)
+        starting_time = time.time()
+        self.typed_text = input("\n")
+        self.result_time = time.time() - starting_time
+        print(self.typed_text)
+        print(self.result_time)
 
     def check_text(self):
         pass
 
-    def end_typing(self):
-        pass
+    def print_result(self, mode):
+        if self.mode == "default":
+            print(mode)
 
