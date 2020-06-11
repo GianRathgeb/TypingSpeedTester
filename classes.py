4b2a5517d9224727d844b4799edc7596cd3d6637
gianrathgeb <gian.rathgeb@bbz-sh.ch>
2020-06-11 22:44:34 +0200

    very simple check_text function

--- classes.py
+++ classes.py
@@ -5,10 +5,13 @@ class SpeedTester:
     def __init__(self, text):
         self.text = text
         self.input = ""
+        self.fails = 0
 
     def start_program(self):
         if self.wait("Press the <ENTER> key to start typing:\n" + str(self.text)):
             self.start_typing()
+            self.end_typing()
+            self.check_text()
 
     def wait(self, text):
         wait = input(text)
@@ -19,13 +22,20 @@ class SpeedTester:
     def start_typing(self):
         self.starting_time = time.time()
         self.input = input("\n")
+
+
+    def check_text(self):
+        for i in range (0, len(self.input) -1 ):
+            if self.text[i] != self.input[i]:
+                self.fails += 1
+                # TODO Write function to check if text is correct and how many fails
+            else:
+                pass # Do nothing
+        print(self.fails)
+                
+
+    def end_typing(self):
         self.time = time.time() - self.starting_time
         print(self.input)
         print(self.time)
-
-    def check_text(self):
-        pass
-
-    def end_typing(self):
-        pass
 
